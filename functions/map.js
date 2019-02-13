638e89ab1a750fc2473d034cb748a7017e970533
Chris Freeman <cc.freem@yahoo.com>
2019-02-13 09:50:45 -0800

    Create map payload (#15)
    * fix for lack of suggestion, and update deployment files for ui
    
    * create script to generate geocode data for office locations
    
    * initial working custom payload
    
    * add type: 'map' to payload
    
    * remove map type declaration
    
    * send map payload as a string
    
    * rename long to lng
    
    * delete geoInfo from scripts directory

---
+++ functions/map.js
@@ -0,0 +1,17 @@
+const { Payload } = require('dialogflow-fulfillment')
+const { locations } = require('./geoInfo.js')
+
+exports.mapRoot = async agent => {
+  const mapPayload = JSON.stringify(locations)
+  await agent.add(`Here is an interactive map of all of our locations!`)
+  await agent.add(
+    new Payload(
+      agent.UNSPECIFIED,
+      { mapPayload },
+      {
+        sendAsMessage: true,
+        rawPayload: true,
+      }
+    )
+  )
+}
