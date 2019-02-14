326fd9588fcd200b19691e2afc1ff16eac1af782
Chris Freeman <cc.freem@yahoo.com>
2019-02-14 11:11:23 -0800

    update to chatframe with map component

--- interface/src/App.js
+++ interface/src/App.js
@@ -3,16 +3,23 @@ import Chatframe from '@cambriasolutions/chatframe'
 import './App.css'
 
 const dfWebhookOptions = {
-  eventUrl:
-    'https://us-central1-mdhs-csa-stage.cloudfunctions.net/eventRequest',
-  textUrl: 'https://us-central1-mdhs-csa-stage.cloudfunctions.net/textRequest',
+  eventUrl: 'https://us-central1-mdhs-csa-dev.cloudfunctions.net/eventRequest',
+  textUrl: 'https://us-central1-mdhs-csa-dev.cloudfunctions.net/textRequest',
 }
 
 const policyText =
   'Please do not enter any personally identifiable information such as SSN, Date of Birth or last name.'
 
+const googleMapsKey = process.env.REACT_APP_GOOGLE_MAPS_KEY
+
+const centerCoordinates = {
+  lat: 32.777025,
+  lng: -89.543724,
+}
+
 class App extends Component {
   render() {
+    console.log(process.env)
     return (
       <Chatframe
         primaryColor='#3bafbf'
@@ -23,6 +30,8 @@ class App extends Component {
         fullscreen={false}
         initialActive={false}
         policyText={policyText}
+        googleMapsKey={googleMapsKey}
+        centerCoordinates={centerCoordinates}
       />
     )
   }
