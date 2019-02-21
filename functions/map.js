e76b2da04cd1b8092f27c408c5287e4c1e55e314
Chris Freeman <cc.freem@yahoo.com>
2019-02-21 14:41:46 -0800

    create handler for geolocation intent

--- functions/map.js
+++ functions/map.js
@@ -2,16 +2,38 @@ const { Payload } = require('dialogflow-fulfillment')
 const { locations } = require('./geoInfo.js')
 
 exports.mapRoot = async agent => {
-  const mapPayload = JSON.stringify(locations)
-  await agent.add(`Here is an interactive map of all of our locations!`)
-  await agent.add(
-    new Payload(
-      agent.UNSPECIFIED,
-      { mapPayload },
-      {
-        sendAsMessage: true,
-        rawPayload: true,
-      }
+  try {
+    await agent.add(
+      `I can help located the nearest child support office to you, what is your address?`
     )
-  )
+    await agent.context.set({
+      name: 'waiting-maps-deliver-map',
+      lifespan: 2,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+exports.mapDeliverMap = async agent => {
+  try {
+    const userAddress = agent.parameters.userAddress
+    const userCity = agent.parameters.userCity
+    const currentLocation = { userAddress, userCity }
+    const mapInfo = [currentLocation, locations]
+    const mapPayload = JSON.stringify(mapInfo)
+    await agent.add(`Here is an interactive map of all of our locations!`)
+    await agent.add(
+      new Payload(
+        agent.UNSPECIFIED,
+        { mapPayload },
+        {
+          sendAsMessage: true,
+          rawPayload: true,
+        }
+      )
+    )
+  } catch (error) {
+    console.error(error)
+  }
 }
