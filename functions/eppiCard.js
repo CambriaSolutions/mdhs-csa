309b4ddc0b06e61922d4d2987bff58dbfdda8fc2
Chris Freeman <cc.freem@yahoo.com>
2019-02-26 13:49:52 -0800

    initial scaffold of eppi convo

---
+++ functions/eppiCard.js
@@ -0,0 +1,93 @@
+const { Suggestion, Card } = require('dialogflow-fulfillment')
+const { handleEndConversation } = require('./globalFunctions.js')
+
+exports.eppiRoot = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiGetCard = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiActivateCard = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiFees = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiNotifications = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiReplaceReport = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiFAQ = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiPaymentHistory = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiUseCard = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiWithdrawCash = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiSurcharge = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiLearnMore = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+exports.eppiBalanceDenial = async agent => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
