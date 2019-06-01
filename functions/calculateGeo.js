eec0777ee6d67cec7376ad1b98011f3b3a6688c1
Jade Tran <jtran@cambriasolutions.com>
2019-05-31 19:24:46 -0700

    removed geolib and use distance matrix api

--- functions/calculateGeo.js
+++ functions/calculateGeo.js
@@ -1,19 +1,17 @@
 require('dotenv').config()
 const URL = require('url').URL
 const fetch = require('node-fetch')
-const geolib = require('geolib')
+const mapsKey = process.env.GOOGLE_MAPS_KEY
 
 exports.getGeocode = async address => {
   if (!address) {
     return
   }
 
-  const apiKey = process.env.GOOGLE_MAPS_KEY
-
   const url = new URL('https://maps.googleapis.com/maps/api/geocode/json'),
     params = {
       address,
-      key: apiKey,
+      key: mapsKey,
     }
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
   const response = await fetch(url)
@@ -27,32 +25,43 @@ exports.getGeocode = async address => {
   }
 }
 
-exports.getNearestThreeLocations = (currentCoordinates, locations) => {
+exports.getNearestThreeLocations = async (currentCoordinates, locations) => {
   if (currentCoordinates !== null) {
-    const results = locations.map(function(place) {
-      const distance = geolib.getDistance(
-        { lat: currentCoordinates.lat, lng: currentCoordinates.lng },
-        {
-          lat: place.lat,
-          lng: place.lng,
-        },
-        '0.01'
-      )
-      const distanceMiles = geolib.convertDistance(distance, 'mi').toFixed(2)
-      return {
-        lat: place.lat,
-        lng: place.lng,
-        street: place.addressComponents.street,
-        city: place.addressComponents.city,
-        placeId: place.placeId,
-        distance: distanceMiles,
-      }
-    })
+    const results = await Promise.all(
+      locations.map(async function(place) {
+        const url = new URL(
+            'https://maps.googleapis.com/maps/api/distancematrix/json'
+          ),
+          params = {
+            units: 'imperial',
+            origins: `${currentCoordinates.lat},${currentCoordinates.lng}`,
+            destinations: `${place.lat},${place.lng}`,
+            key: mapsKey,
+          }
+        Object.keys(params).forEach(key =>
+          url.searchParams.append(key, params[key])
+        )
+
+        const response = await fetch(url)
+        const json = await response.json()
+
+        if (json.rows.length !== 0) {
+          const distance = json.rows[0].elements[0].distance.text
+          return {
+            lat: place.lat,
+            lng: place.lng,
+            street: place.addressComponents.street,
+            city: place.addressComponents.city,
+            placeId: place.placeId,
+            distance,
+          }
+        }
+      })
+    )
     const sortedResults = results.sort(
       (a, b) => parseFloat(a.distance) - parseFloat(b.distance)
     )
-    const getNearestThreeLocations = sortedResults.slice(0, 3)
-
-    return getNearestThreeLocations
+    const nearestThreeLocations = sortedResults.slice(0, 3)
+    return nearestThreeLocations
   }
 }
