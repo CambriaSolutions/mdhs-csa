8e5a1ef46e503809ec6a3e845a8a05b0c8c50162
Chris Freeman <cc.freem@yahoo.com>
2019-09-19 12:57:59 -0700

    add link to privacy statement

--- functions/notChildSupport.js
+++ functions/notChildSupport.js
@@ -43,7 +43,7 @@ exports.handleChildSupportRetry = async agent => {
       `Great! I can assist you by providing general information about the child support program or by directing common child support requests to the appropriate MDHS team for handling. The information I provide is not legal advice. MDHS does not provide legal representation. Also, please do not enter SSN or DOB in at any time during your conversations.`
     )
     await agent.add(
-      `By clicking "I Acknowledge" below you are acknowledging receipt and understanding of these statements and the MDHS Website Disclaimers, Terms, and Conditions found here, and that you wish to continue.`
+      `By clicking "I Acknowledge" below you are acknowledging receipt and understanding of these statements and the MDHS Website Disclaimers, Terms, and Conditions found <a href="https://www.mdhs.ms.gov/privacy-disclaimer/" target="_blank">here</a>, and that you wish to continue.`
     )
     await agent.add(new Suggestion('I Acknowledge'))
     await disableInput(agent)
