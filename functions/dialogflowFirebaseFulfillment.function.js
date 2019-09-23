45a326052140d10dc4c61a26790a4b49212dce8b
Jade Tran <jtran@cambriasolutions.com>
2019-09-23 14:03:30 -0700

    bold please try rephrasing

--- functions/dialogflowFirebaseFulfillment.function.js
+++ functions/dialogflowFirebaseFulfillment.function.js
@@ -328,7 +328,7 @@ exports = module.exports = functions
     const fallback = async agent => {
       try {
         await agent.add(
-          `I’m sorry, I’m not familiar with that right now, but I’m still learning! I can help answer a wide variety of questions about Child Support; please try rephrasing or click on one of the options provided. If you need immediate assistance, please contact the Child Support Call Center at <a href="tel:+18778824916">877-882-4916</a>.`
+          `I’m sorry, I’m not familiar with that right now, but I’m still learning! I can help answer a wide variety of questions about Child Support; <strong>please try rephrasing</strong> or click on one of the options provided. If you need immediate assistance, please contact the Child Support Call Center at <a href="tel:+18778824916">877-882-4916</a>.`
         )
         await agent.add(new Suggestion(`Home`))
 
