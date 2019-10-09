0dc81294de689cbc202a826e1faebc8f40145e59
Chris Freeman <cc.freem@yahoo.com>
2019-10-09 12:43:19 -0700

    include income withholding and remove underline

--- functions/enforcement.js
+++ functions/enforcement.js
@@ -18,6 +18,7 @@ exports.enforcementRoot = async agent => {
     await agent.add(new Suggestion('Passport Revocation'))
     await agent.add(new Suggestion('Credit Bureau Reporting'))
     await agent.add(new Suggestion('Unemployment Benefits'))
+    await agent.add(new Suggestion('Income Withholding'))
     await agent.add(new Suggestion('Bankruptcy'))
   } catch (err) {
     console.error(err)
@@ -63,7 +64,7 @@ exports.enforcementLicenseSuspension = async agent => {
       'Generally, after 30 days of non-payment a license suspension notice will automatically be sent to the parent who owes support. The parent who owes support has 90 days to become current or enter into a payment arrangement to avoid suspension. If the parent who owes support does not comply, the license is subject to suspension.'
     )
     await agent.add(
-      'If the parent who owes support previously entered into a <u>court-approved</u> payment arrangement and fails to make payments in accordance with that arrangement, a notice is not required and the 90 days does not apply. The license is subject to immediate suspension.'
+      'If the parent who owes support previously entered into a court-approved payment arrangement and fails to make payments in accordance with that arrangement, a notice is not required and the 90 days does not apply. The license is subject to immediate suspension.'
     )
     await handleEndConversation(agent)
   } catch (err) {
@@ -80,7 +81,7 @@ exports.enforcementLicenseSuspension = async agent => {
 exports.enforcementLicenseSuspensionNonCompliance = async agent => {
   try {
     await agent.add(
-      'If the parent who owes support previously entered into a <u>court-approved</u> payment arrangement and fails to make payments in accordance with that arrangement, a notice is not required and the 90 days does not apply. The license is subject to immediate suspension.'
+      'If the parent who owes support previously entered into a court-approved payment arrangement and fails to make payments in accordance with that arrangement, a notice is not required and the 90 days does not apply. The license is subject to immediate suspension.'
     )
     await handleEndConversation(agent)
   } catch (err) {
