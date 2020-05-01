56b8ee96b0f06c8b0669b3875bba31d11b09e2d9
Bryand Acosta <acostab@infinity-software.com>
2020-05-01 15:47:45 -0400

    Restoring all intents and user phrases from PROD

--- functions/stimulusCheck.js
+++ functions/stimulusCheck.js
@@ -2,7 +2,7 @@ const { handleEndConversation } = require('./globalFunctions')
 
 exports.stimulusCheck = async agent => {
     try {
-        await agent.add(`To learn more about the Economic Impact (Stimulus) Payments and collecting child support arrears through the Treasury Offset Program, click here.`)
+        await agent.add(`To learn more about the Economic Impact (Stimulus) Payments and collecting child support arrears through the Treasury Offset Program, <a href="https://www.mdhs.ms.gov/faqs-on-economic-impact-stimulus-payments-and-the-treasury-offset-program/" target="_blank">click here.</a>`)
 
         await handleEndConversation(agent)
     } catch (err) {
