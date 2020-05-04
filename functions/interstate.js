9330ec6b573a09de076b9bd9da133f1fb4c69ff9
Bryand Acosta <acostab@infinity-software.com>
2020-05-04 12:59:24 -0400

    Updating the dev environment to be a snapshot of PROD but with the tax offset link and stimulus check content.

--- functions/interstate.js
+++
@@ -1,9 +0,0 @@
-const { tbd } = require('./globalFunctions.js')
-
-exports.interstate = async agent => {
-    try {
-        await tbd(agent)
-    } catch (err) {
-        console.log(err)
-    }
-}
