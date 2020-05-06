b1d230ed6dfc7ccbcbcd9647e7e994356c0708ce
Bryand Acosta <acostab@infinity-software.com>
2020-05-06 11:13:25 -0400

    Adding a necessary context to enforcement contempt

--- functions/enforcement.js
+++ functions/enforcement.js
@@ -381,6 +381,10 @@ exports.enforcementContempt = async agent => {
       `If you would like to submit a request for contempt action on your case, please select "Submit request for contempt" below.`
     )
     await agent.add(new Suggestion('Request Contempt Action'))
+    await agent.context.set({
+      name: 'waiting-support-type',
+      lifespan: 3,
+    })
   } catch (err) {
     console.error(err)
   }
