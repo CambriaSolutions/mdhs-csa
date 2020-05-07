aba887e9c213d85f04479bf4187885e80f972041
Brian Havice <haviceb@infinity-software.com>
2020-05-07 13:14:40 -0400

    Successful DF export

---
+++ scripts/exportDialogFlow.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const fs = require('fs')
+const dialogflow = require('dialogflow')
+
+const client = new dialogflow.v2.AgentsClient()
+
+client.exportAgent({ parent: `projects/${process.env.PROJECT_ID}` })
+    .then(responses => {
+        const [operation, initialApiResponse] = responses;
+
+        // Operation#promise starts polling for the completion of the LRO.
+        return operation.promise();
+    })
+    .then(responses => {
+        fs.writeFile(`export_${process.env.PROJECT_ID}_${new Date().toISOString().slice(0, 10)}.zip`, responses[0].agentContent, (err) => {
+            if (err)
+                console.error(err)
+            console.log("Completed");
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    });
