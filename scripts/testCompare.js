1b0a3822458cb97d8ad9977cfbffe4d2635176df
Brian Havice <haviceb@infinity-software.com>
2020-05-13 09:35:31 -0400

    Removing test

--- scripts/testCompare.js
+++
@@ -1,4 +0,0 @@
-const IntentComparator = require('./intentComparator.js');
-
-var comparator = new IntentComparator('calc-root', '../agent/intents', './exported/intents');
-console.log(comparator.compare());
