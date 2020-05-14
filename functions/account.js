e58dd5657896f1002aa216891988dc0f0b2ab4de
Brian Havice <haviceb@infinity-software.com>
2020-05-14 09:10:51 -0400

    Added href for phone number

--- functions/account.js
+++ functions/account.js
@@ -3,7 +3,7 @@ const { handleEndConversation, tbd } = require('./globalFunctions.js')
 exports.accountBalance = async agent => {
   try {
     await agent.add(
-      'To avoid fees, you may check the account balance of your debit card/EPPIcard online by visiting <a href="http://www.eppicard.com" target="_blank">www.eppicard.com</a> and creating an account. You may also check the balance on your debit card/EPPIcard 24 hours a day, 7 days a week by calling customer service at 1-866-461-4095. Please note that fees may apply when calling customer service.'
+      'To avoid fees, you may check the account balance of your debit card/EPPIcard online by visiting <a href="http://www.eppicard.com" target="_blank">www.eppicard.com</a> and creating an account. You may also check the balance on your debit card/EPPIcard 24 hours a day, 7 days a week by calling customer service at <a href="tel:+18664614095">1-866-461-4095</a>. Please note that fees may apply when calling customer service.'
     )
     await handleEndConversation(agent)
   } catch (err) {
