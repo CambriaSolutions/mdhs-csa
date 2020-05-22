8fac29496b75eae87390b2127afe1035df41804c
Brian Havice <haviceb@infinity-software.com>
2020-05-22 17:15:43 -0400

    Updated text request to allow for multiple dialogflow instances for multiple subject matters

--- functions/textRequest.function.js
+++ functions/textRequest.function.js
@@ -15,8 +15,8 @@ const runtimeOpts = {
 
 // Register SMD, always
 const subjectMatterInstancesMap = new Map();
-const smdKnowledgeBase = 'smd';
-subjectMatterInstancesMap.set(smdKnowledgeBase, require(process.env.SMD_KEY_FILE));
+const subjectMatterDiscriminator = 'smd';
+subjectMatterInstancesMap.set(subjectMatterDiscriminator, require(process.env.SMD_KEY_FILE));
 
 // Register additional knowledge bases
 const registeredSubjectMatters = require('./registeredSubjectMatters.js');
@@ -32,23 +32,29 @@ registeredSubjectMatters().forEach((value, key) => {
   });
 });
 
+// Retrieve the subject matter the user is working with
 const getSessionSubjectMatter = async (sessionId) => {
   const sessionInfo = await db.collection('sessions').doc(sessionId).get();
+
+  // If not subject matter has been set, forward to the discriminator
   if (sessionInfo === undefined) {
-    return smdKnowledgeBase;
+    await setSessionSubjectMatter(subjectMatterDiscriminator);
+    return subjectMatterDiscriminator;
   } else {
     return sessionInfo.subjectMatter;
   }
 };
 
-const setSessionSubjectMatter = async (sessionId, knowledgeBase) => {
+// Using the sessionId, record which subject matter the user is working with
+const setSessionSubjectMatter = async (sessionId, subjectMatter) => {
   const sessionInfo = {
-    "subjectMatter" : knowledgeBase
+    "subjectMatter" : subjectMatter
   };
   
   await db.collection('sessions').doc(sessionId).set(sessionInfo);
 };
 
+// Submit the query to a target dialogFlow instance
 const dispatchToDialogFlow = async (subjectMatterInstance, sessionId, query) => {
   return new Promise((resolve, reject) => {
     const sessionPath = sessionClient.sessionPath(subjectMatterInstance.projectId, sessionId);
@@ -64,6 +70,37 @@ const dispatchToDialogFlow = async (subjectMatterInstance, sessionId, query) => 
         reject(err);
       });
   });
+};
+
+// Infer if the intent on the dialogFlow response indicates if the user wants to change subject matter
+const hasSubjectMatterChangeBeenRequested = (dialogFlowResponse) => {
+  const intent = dialogFlowResponse.queryResult.intent.displayName;
+  return intent.startsWith('change-subject-matter');
+};
+
+// Extract from the dialogFlow response which subject matter is being targeted. 
+const determineTargetSubjectMatter = (dialogFlowResponse) => {
+  const intent = dialogFlowResponse.queryResult.intent.displayName;
+  const targetSubjectMatter = intent.split('-').pop();
+  return targetSubjectMatter;
+};
+
+// Examine the request and session to determine how to handle the query
+const processTextRequest = async (sessionId, query) => {
+  // Using the current session subject matter, submit the request to dialogFlow
+  let subjectMatter = await getSessionSubjectMatter(sessionId);
+  let subjectMatterInstance = subjectMatterInstancesMap.get(subjectMatter);
+  let dialogFlowResponse = await dispatchToDialogFlow(subjectMatterInstance, sessionId, query);
+
+  // If the intent implies we need to switch subject matter, determine the target subject matter and query for a greeting message.
+  if (hasSubjectMatterChangeBeenRequested(dialogFlowResponse)) {
+    subjectMatter = determineTargetSubjectMatter(dialogFlowResponse);
+    subjectMatterInstance = subjectMatterInstancesMap.get(subjectMatter);
+    await setSessionSubjectMatter(sessionId, subjectMatter);
+    dialogFlowResponse = await dispatchToDialogFlow(subjectMatterInstance, sessionId, 'subject matter changed');
+  }
+
+  return dialogFlowResponse;
 }
 
 exports.textRequest = functions
@@ -79,21 +116,8 @@ exports.textRequest = functions
 
       const query = req.query.query;
       const sessionId = req.query.uuid;
+      const dialogFlowResponse = await processTextRequest(sessionId, query);      
 
-      const subjectMatter = await getSessionSubjectMatter(sessionId);
-      const subjectMatterInstance = subjectMatterInstancesMap.get(subjectMatter);
-      const dialogFlowResponse = await dispatchToDialogFlow(subjectMatterInstance, sessionId, query);
-
-      return sessionClient
-        .detectIntent(dfRequest)
-        .then(responses => {
-          // return responses[0]
-          responses[0].session = sessionPath
-          res.json(responses[0])
-        })
-        .catch(err => {
-          console.error('textRequest.function.js: ', err)
-          return `Dialogflow error: ${err}`
-        })
+      return res.json(dialogFlowResponse);
     })
-  })
+  });
