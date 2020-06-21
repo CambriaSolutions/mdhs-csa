2d68b712515b967d5c2185e821eb001fb64e0533
Bryand Acosta <acostab@infinity-software.com>
2020-06-21 16:31:09 -0400

    Wrapping fulfillment function in try catches to help us identify issues

--- functions/dialogflowFirebaseFulfillment.function.js
+++ functions/dialogflowFirebaseFulfillment.function.js
@@ -17,40 +17,49 @@ const runtimeOpts = {
 exports = module.exports = functions
   .runWith(runtimeOpts)
   .https.onRequest(async (request, response) => {
-    console.log(
-      'Dialogflow Request headers: ' + JSON.stringify(request.headers)
-    )
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
-    req({
-      method: 'POST',
-      uri: process.env.ANALYTICS_URI,
-      body: request.body,
-      json: true,
-    })
+    try {
+      console.log(
+        'Dialogflow Request headers: ' + JSON.stringify(request.headers)
+      )
+      console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
 
-    let intentHandlers = {
-      ...globalIntentHandlers,
-      ...commonIntentHandlers,
-      ...childSupportIntentHandlers,
-      ...tanfIntentHandlers,
-      ...snapIntentHandlers
+      let intentHandlers = {
+        ...globalIntentHandlers,
+        ...commonIntentHandlers,
+        ...childSupportIntentHandlers,
+        ...tanfIntentHandlers,
+        ...snapIntentHandlers
+      }
+
+      // List of intents what will reset the back button context
+      const resetBackIntentList = [
+        'Default Welcome Intent',
+        'cse-support-submit-issue',
+      ]
+
+      const agent = new WebhookClient({ request, response })
+
+      await backIntent(agent, intentHandlers, resetBackIntentList)
+      await home(agent, intentHandlers, [
+        'Default Welcome Intent',
+        'restart-conversation',
+        'global-restart',
+        'acknowledge-privacy-statement'
+      ])
+
+      await agent.handleRequest(new Map(Object.entries(intentHandlers)))
+    } catch (e) {
+      console.error(e)
     }
 
-    // List of intents what will reset the back button context
-    const resetBackIntentList = [
-      'yes-child-support',
-      'Default Welcome Intent',
-      'support-submit-issue',
-    ]
-
-    const agent = new WebhookClient({ request, response })
-    await backIntent(agent, intentHandlers, resetBackIntentList)
-    await home(agent, intentHandlers, [
-      'Default Welcome Intent',
-      'restart-conversation',
-      'global-restart',
-      'acknowledge-privacy-statement',
-    ])
-
-    await agent.handleRequest(new Map(Object.entries(intentHandlers)))
+    try {
+      req({
+        method: 'POST',
+        uri: process.env.ANALYTICS_URI,
+        body: request.body,
+        json: true,
+      })
+    } catch (e) {
+      console.error(e)
+    }
   })
