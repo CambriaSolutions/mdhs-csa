2195f36d0a88e8707e88c689063e119e264a4833
Bryand Acosta <acostab@infinity-software.com>
2020-06-21 16:30:40 -0400

    #241 - Eligibility Checker for TANF and SNAP

--- functions/snapIntentHandlers.js
+++ functions/snapIntentHandlers.js
@@ -1,5 +1,7 @@
 const { snapRoot } = require('./intentHandlers/snap/snapRoot.js')
+const { eligibilityChecker } = require('./intentHandlers/common/eligibilityChecker.js')
 
 module.exports = {
   'snap-root': snapRoot,
+  'snap-eligibilityChecker': eligibilityChecker
 }
--- functions/tanfIntentHandlers.js
+++ functions/tanfIntentHandlers.js
@@ -1,5 +1,7 @@
 const { tanfRoot } = require('./intentHandlers/tanf/tanfRoot.js')
+const { eligibilityChecker } = require('./intentHandlers/common/eligibilityChecker.js')
 
 module.exports = {
   'tanf-root': tanfRoot,
+  'tanf-eligibilityChecker': eligibilityChecker
 }
