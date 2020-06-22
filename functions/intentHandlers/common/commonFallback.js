cd14d4af1f61f7be8c88143ff2bdabb18e1a182b
Brian Havice <haviceb@infinity-software.com>
2020-06-22 15:50:29 -0400

    Checking for context directly

--- functions/intentHandlers/common/commonFallback.js
+++ functions/intentHandlers/common/commonFallback.js
@@ -1,18 +1,6 @@
 const { defaultFallback } = require('../globalFunctions')
 const { autoMlFallback } = require('./categorizeAndPredict')
 
-const determineSubjectMatter = async agent => {
-  const cse = agent.context.get('cse-subject-matter')
-  console.log(`CSE Context: ${JSON.stringify(cse)}`)
-  if (cse) {
-    console.log('Determined context to be CSE.')
-    return 'cse'
-  } else {
-    console.log('Determined context to be NONE.')
-    return 'none'
-  }
-}
-
 exports.noneOfThese = async agent => {
   // TODO For now, treat as unhandled w/o ML.
   // In the future record the phrase, category, and suggestions not selected.
@@ -24,7 +12,7 @@ exports.noneOfThese = async agent => {
 }
 
 exports.commonFallback = async agent => {
-  if (determineSubjectMatter(agent) === 'cse') {
+  if (agent.context.get('cse-subject-matter')) {
     return autoMlFallback(agent)
   } else {
     return defaultFallback(agent)
