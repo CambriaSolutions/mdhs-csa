ea5909c73f399b98bf9a00eaec248568afecc2ef
Bryand Acosta <acostab@infinity-software.com>
2020-06-22 11:56:08 -0400

    #268 - Document upload for TANF and SNAP

--- functions/commonIntentHandlers.js
+++ functions/commonIntentHandlers.js
@@ -1,9 +1,11 @@
 const { commonFallback, noneOfThese } = require('./intentHandlers/common/commonFallback.js')
 const { mapRoot, mapDeliverMap } = require('./intentHandlers/common/map.js')
+const { docUpload } = require('./intentHandlers/common/docUpload.js')
 
 module.exports = {
   'none-of-these': noneOfThese,
   'Default Fallback Intent': commonFallback,
+  'doc-upload': docUpload,
   'map-root': mapRoot,
   'map-deliver-map': mapDeliverMap
 }
