f9e673e4f90abdfa24e6e0df25d0fa2a2f016cdd
Bryand Acosta <acostab@infinity-software.com>
2020-07-20 11:13:24 -0400

    #361 - Office locations for each subject matter and some agent folder cleanup to match what is in dialogflow dev

--- functions/tanfIntentHandlers.js
+++ functions/tanfIntentHandlers.js
@@ -1,9 +1,13 @@
 const { tanfRoot } = require('./intentHandlers/tanf/tanfRoot.js')
 const { eligibilityChecker } = require('./intentHandlers/common/eligibilityChecker.js')
 const { tanfApplication } = require('./intentHandlers/tanf/tanfApplication.js')
+const { mapRoot } = require('./intentHandlers/common/map.js')
 
 module.exports = {
   'tanf-root': tanfRoot,
   'tanf-eligibilityChecker': eligibilityChecker,
-  'tanf-application': tanfApplication
+  'tanf-application': tanfApplication,
+
+  // Map
+  'tanf-map-root': mapRoot
 }
