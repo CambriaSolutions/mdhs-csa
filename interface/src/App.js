c3805e73109a99c7e3e42d202348a574e9742b19
Bryand Acosta <acostab@infinity-software.com>
2020-07-22 12:34:01 -0400

    Undid a minor change I tried earlier

--- interface/src/App.js
+++ interface/src/App.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react'
 import Chatframe from '@cambriasolutions/chatframe'
 import './App.css'
 
+const options = {
+  eventUrl: process.env.REACT_APP_EVENT_URL,
+  textUrl: process.env.REACT_APP_TEXT_URL
+}
+
+const feedbackUrl = process.env.REACT_APP_FEEDBACK_URL
+
+const privacyPolicy =
+  'Please do not enter any personally identifiable information such as SSN or Date of Birth'
+
+const mapConfig = {
+  googleMapsKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+  centerCoordinates: {
+    lat: 32.777025,
+    lng: -89.543724,
+  },
+}
+
+const activationText = 'Talk to Gen'
+
 class App extends Component {
-  options = {
-    eventUrl: process.env.REACT_APP_EVENT_URL,
-    textUrl: process.env.REACT_APP_TEXT_URL
-  }
-
-  feedbackUrl = process.env.REACT_APP_FEEDBACK_URL
-
-  privacyPolicy =
-    'Please do not enter any personally identifiable information such as SSN or Date of Birth'
-
-  mapConfig = {
-    googleMapsKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-    centerCoordinates: {
-      lat: 32.777025,
-      lng: -89.543724,
-    },
-  }
-
-  activationText = 'Talk to Gen'
-
   render() {
     console.log('Process.env', process.env)
 
@@ -33,13 +33,13 @@ class App extends Component {
         headerColor='#6497AD'
         title='Gen (TEST CHAT)'
         client='Dialogflow'
-        clientOptions={this.options}
+        clientOptions={options}
         fullscreen={false}
         initialActive={false}
-        policyText={this.privacyPolicy}
-        mapConfig={this.mapConfig}
-        activationText={this.activationText}
-        feedbackUrl={this.feedbackUrl}
+        policyText={privacyPolicy}
+        mapConfig={mapConfig}
+        activationText={activationText}
+        feedbackUrl={feedbackUrl}
       />
     )
   }
