b0d520d6fcc7a3c57825a2b6c823a87d34caa219
Bryand Acosta <acostab@infinity-software.com>
2020-07-22 12:31:59 -0400

    #354 - fixed issue with office locations and the message 'for development only'

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,45 +1,45 @@
 import React, { Component } from 'react'
 import Chatframe from '@cambriasolutions/chatframe'
 import './App.css'
-// TODO NEED TO MAKR THIS NOT BE HARDCODED
-const dfWebhookOptions = {
-  eventUrl:
-    'https://us-central1-mdhs-csa-isd-273818.cloudfunctions.net/eventRequest',
-  textUrl: 'https://us-central1-mdhs-csa-isd-273818.cloudfunctions.net/textRequest',
-}
-
-const policyText =
-  'Please do not enter any personally identifiable information such as SSN or Date of Birth.'
-
-const feedbackUrl =
-  'https://us-central1-webchat-analytics-dev.cloudfunctions.net/storeFeedback'
-
-export const mapConfig = {
-  googleMapsKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-  centerCoordinates: {
-    lat: 32.777025,
-    lng: -89.543724,
-  },
-}
-
-export const activationText = 'Talk to Gen'
 
 class App extends Component {
+  options = {
+    eventUrl: process.env.REACT_APP_EVENT_URL,
+    textUrl: process.env.REACT_APP_TEXT_URL
+  }
+
+  feedbackUrl = process.env.REACT_APP_FEEDBACK_URL
+
+  privacyPolicy =
+    'Please do not enter any personally identifiable information such as SSN or Date of Birth'
+
+  mapConfig = {
+    googleMapsKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+    centerCoordinates: {
+      lat: 32.777025,
+      lng: -89.543724,
+    },
+  }
+
+  activationText = 'Talk to Gen'
+
   render() {
+    console.log('Process.env', process.env)
+
     return (
       <Chatframe
         primaryColor='#6497AD'
         secondaryColor='#6497AD'
         headerColor='#6497AD'
-        title='Gen'
+        title='Gen (TEST CHAT)'
         client='Dialogflow'
-        clientOptions={dfWebhookOptions}
+        clientOptions={this.options}
         fullscreen={false}
         initialActive={false}
-        policyText={policyText}
-        mapConfig={mapConfig}
-        feedbackUrl={feedbackUrl}
-        activationText={activationText}
+        policyText={this.privacyPolicy}
+        mapConfig={this.mapConfig}
+        activationText={this.activationText}
+        feedbackUrl={this.feedbackUrl}
       />
     )
   }
