509c8bdc63fdc4bde5621ab877bcfaf92486b06d
Bryand Acosta <acostab@infinity-software.com>
2020-07-24 16:00:28 -0400

    #390 - pebt content

--- functions/tanfIntentHandlers.js
+++ functions/tanfIntentHandlers.js
@@ -2,11 +2,13 @@ const { tanfRoot } = require('./intentHandlers/tanf/tanfRoot.js')
 const { eligibilityChecker } = require('./intentHandlers/common/eligibilityChecker.js')
 const { tanfApplication } = require('./intentHandlers/tanf/tanfApplication.js')
 const { mapRoot } = require('./intentHandlers/common/map.js')
+const { pebtRoot } = require('./intentHandlers/common/pebt')
 
 module.exports = {
   'tanf-root': tanfRoot,
   'tanf-eligibilityChecker': eligibilityChecker,
   'tanf-application': tanfApplication,
+  'tanf-pebt-root': pebtRoot,
 
   // Map
   'tanf-map-root': mapRoot
