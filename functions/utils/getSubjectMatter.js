5af1b712014451980ec8a1c6116de34c396c0cb7
Bryand Acosta <acostab@infinity-software.com>
2020-08-06 17:38:39 -0400

    Fixed bug that caused office locations to break

--- functions/utils/getSubjectMatter.js
+++ functions/utils/getSubjectMatter.js
@@ -5,5 +5,5 @@ const { subjectMatterContexts } = require('../constants/constants.js')
 module.exports = (agent) => {
   const subjectMatterContext = find(agent.contexts, context => subjectMatterContexts.includes(context.name))
 
-  return subjectMatterContext ? head(split(subjectMatterContext.name)) : ''
+  return subjectMatterContext ? head(split(subjectMatterContext.name, '-')) : ''
 }
