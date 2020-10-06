df6f249c6edbcc9692ccddf24f59d2cf97e15b77
Bryand Acosta <acostab@infinity-software.com>
2020-10-06 15:29:48 -0400

    #484 updating scripts package dependencies and restructuring folder a bit

---
+++ scripts/DELETE/getUnhandled.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const fs = require('fs')
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+const db = admin.firestore()
+
+
+
+
+
+
+const getMetrics = async (subjectMatter) => {
+  const snap = await db.collection(`subjectMatters/${subjectMatter}/metrics`).get()
+
+  const fallbacksMap = new Map()
+  snap.docs.forEach(doc => {
+    const metric = doc.data()
+    //console.log(metric)
+    if (metric.fallbackTriggeringQueries) {
+      metric.fallbackTriggeringQueries.forEach(fallbackTriggeringQuery => {
+        if (!fallbacksMap.has(fallbackTriggeringQuery.queryText)) {
+          fallbacksMap.set(fallbackTriggeringQuery.queryText, fallbackTriggeringQuery.occurrences)
+        } else {
+          const occurrences = fallbacksMap.get(fallbackTriggeringQuery.occurrences)
+          fallbacksMap.set(fallbackTriggeringQuery.queryText, occurrences + fallbackTriggeringQuery.occurrences)
+        }
+      })
+    }
+  })
+
+  return fallbacksMap
+}
+
+const run = async () => {
+  const snap = await getMetrics('snap')
+  fs.writeFileSync('./snap.json', JSON.stringify(Array.from(snap.entries())))
+  const tanf = await getMetrics('tanf')
+  fs.writeFileSync('./tanf.json', JSON.stringify(Array.from(tanf.entries())))
+  //console.log(metrics)
+}
+
+run()
--- scripts/deployment/mlCategories.js
+++ scripts/deployment/mlCategories.js
@@ -1,170 +1,128 @@
 exports.categoriesWithIntents = {
   accountBalance: {
-    intent: 'cse-account-balance',
     suggestionText: 'Account balance',
   },
   accountInformation: {
-    intent: 'cse-accountInformation-root',
     suggestionText: 'Account information'
   },
   addressingCheck: {
-    intent: 'cse-pmts-general-make-payments',
     suggestionText: 'Address check',
   },
   appointments: {
-    intent: 'cse-appts-root',
     suggestionText: 'Appointments'
   },
   arrears: {
-    intent: 'cse-iwoQA-arrears-balance',
     suggestionText: 'Arrears'
   },
   card: {
-    intent: 'cse-eppi-get-card',
     suggestionText: 'EPPI card'
   },
   caseNumber: {
-    intent: 'cse-caseQA-general',
     suggestionText: 'Case Number',
   },
   caseStatus: {
-    intent: 'cse-caseQA-general',
     suggestionText: 'Case Status',
   },
   changeOfInformation: {
-    intent: 'cse-caseQA-change-personal-info',
     suggestionText: 'Change of Information',
   },
   childCare: {
-    intent: 'cse-childCare-root',
     suggestionText: 'Child care'
-  }, // Suggest child care intent
+  },
   complaints: {
-    intent: 'feedback-root',
     suggestionText: 'Complaints'
-  }, // Suggest complaints intent
+  },
   contactHuman: {
-    intent: 'cse-contact-qa-number',
     suggestionText: 'Contact',
   },
   documentation: {
-    intent: 'cse-documentation-root',
     suggestionText: 'Documents'
-  }, // Suggest documentation intent
+  },
   email: {
-    intent: 'cse-email-root',
     suggestionText: 'Email'
-  }, // Suggest email intent
+  },
   emancipation: {
-    intent: 'cse-emancipation-qa-age',
     suggestionText: 'Emancipation',
   },
   employer: {
-    intent: 'cse-employer-root',
     suggestionText: 'Employer'
   },
   enforcement: {
-    intent: 'cse-enforcement-root',
     suggestionText: 'Enforcement'
   },
   estimatePayments: {
-    intent: 'cse-pmt-calc-root',
     suggestionText: 'Estimate payments',
   },
   fax: {
-    intent: 'cse-fax-root',
     suggestionText: 'Fax'
-  }, // Suggest fax intent
+  },
   fee: {
-    intent: 'cse-fee-root',
     suggestionText: 'Fees'
   },
   gratitude: {
-    intent: 'cse-gratitude-root',
     suggestionText: 'Gratitude'
-  }, // Suggest gratitude intent
+  },
   incarceration: {
-    intent: 'cse-support-qa-ncp-prison',
     suggestionText: 'Incarceration',
   },
   interstate: {
-    intent: 'cse-interstate-root',
     suggestionText: 'Interstate Cases'
-  }, // Suggest interstate intent
+  },
   legal: {
-    intent: 'cse-legal-root',
     suggestionText: 'Legal'
-  }, // Suggest legal intent
+  },
   login: {
-    intent: 'cse-login-root',
     suggestionText: 'Login issue'
-  }, // Suggest login intent
+  },
   makePayment: {
-    intent: 'cse-pmts-general-make-payments',
     suggestionText: 'Make payments',
   },
   notReceivedPayment: {
-    intent: 'cse-pmtQA-havent-received',
     suggestionText: 'Haven\'t received payments',
   },
   officeLocations: {
-    intent: 'cse-map-root',
     suggestionText: 'Office locations'
   },
   onlineAction: {
-    intent: 'cse-onlineAction-root',
     suggestionText: 'Online Action'
-  }, // suggest online action intent
+  },
   openCase: {
-    intent: 'cse-open-csc-root',
     suggestionText: 'Open case'
   },
   paternity: {
-    intent: 'cse-geneticTesting-request',
     suggestionText: 'Genetic testing',
   },
   parentCseGuide: {
-    intent: 'cse-support-parentsGuideCSE',
     suggestionText: 'Parent’s Guide to CSE'
   },
   paymentTimelines: {
-    intent: 'cse-paymentTimelines-root',
     suggestionText: 'Payment timelines'
-  }, // Need payment timelines intent
+  },
   phoneNumber: {
-    intent: 'cse-phoneNumber-root',
     suggestionText: 'Phone number'
   },
   refund: {
-    intent: 'cse-refund-root',
     suggestionText: 'Refund'
-  }, // Suggest refund intent
+  },
   snap: {
-    intent: 'cse-snap-root',
     suggestionText: 'SNAP'
-  }, // Suggest snap intent
+  },
   supportRequest: {
-    intent: 'cse-support-root',
     suggestionText: 'Support requests',
   },
   tanf: {
-    intent: 'cse-tanf-root',
     suggestionText: 'TANF'
-  }, // Suggest tanf intent
+  },
   taxes: {
-    intent: 'cse-taxes-root',
     suggestionText: 'Taxes'
   },
   terminate: {
-    intent: 'cse-terminate-root',
     suggestionText: 'Close case'
   },
   verification: {
-    intent: 'cse-verification-root',
     suggestionText: 'Verification'
-  }, // Suggest verification intent
+  },
   visitation: {
-    intent: 'cse-visitation-root',
     suggestionText: 'Visitation'
-  }, // Suggest visitation intent
+  }
 }
