39cf92c3a086c9a21538397a858d016dabf41342
Bryand Acosta <acostab@infinity-software.com>
2020-10-14 13:50:11 -0400

    #498 - added the plugin code to the mdhs repo

---
+++ chatframe/white-listed-pages.php
@@ -0,0 +1,5 @@
+<?php
+
+// Urls containing any of theses elements will allow our chatframe
+// to appear on the page
+ $acceptedPathArray = array("/", "/child-support", "/child-support/", "/economic-assistance", "/economic-assistance/", "/workforce-development", "/workforce-development/");
