b2345c12965b9d71a284f25f515e7091f98ef452
Bryand Acosta <acostab@infinity-software.com>
2020-10-27 10:42:01 -0400

    Minor changes to excel export feature

--- hosting/analytics/src/containers/Settings.js
+++ hosting/analytics/src/containers/Settings.js
@@ -27,6 +27,9 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 // Icons
 import SecurityIcon from '@material-ui/icons/Security'
 import CloudOffIcon from '@material-ui/icons/CloudOff'
+import { IconButton } from '@material-ui/core'
+
+import excelIcon from '../assets/excelIcon.png'
 
 const StyledDiv = styled.div`
   width: 360px;
@@ -91,6 +94,10 @@ const StyledExportButtonRow = styled.div`
   padding-bottom: 16px;
 `
 
+const StyledImg = styled.img`
+  width: 40px;
+`
+
 class Settings extends Component {
   constructor(props) {
     super(props)
@@ -139,9 +146,7 @@ class Settings extends Component {
     let currentSubjectMatterSettings = null
 
     if (this.props.user.dataExport) {
-      let downloadBtnToggle = (
-        <Button variant="outlined" color="primary" onClick={this.props.onExportDownload}>Excel</Button>
-      )
+      let downloadBtnToggle = <IconButton onClick={this.props.onExportDownload} ><StyledImg src={excelIcon} /></IconButton>
       if (this.props.loadingDownload) {
         downloadBtnToggle = <CircularProgress color='primary' />
       }
@@ -149,7 +154,7 @@ class Settings extends Component {
         <div>
           <TitleDiv variant='h6'>Data Export: Unhandled Questions and Feedback</TitleDiv>
           <Divider />
-          <StyledExportText>Download questions that Genbot has been asked, but hasn't been trained to handle yet. Download feedback that your users are saying about the product.</StyledExportText>
+          <StyledExportText>Download questions that Genbot has been asked, but hasn't been trained to handle yet. Download feedback that your users are saying about Genbot.</StyledExportText>
           <StyledExportButtonRow>
             {downloadBtnToggle}
           </StyledExportButtonRow>
