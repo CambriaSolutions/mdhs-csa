c8d02996f2e605cf8b0980096036a80d433e6f15
Brian Havice <haviceb@infinity-software.com>
2020-11-12 15:33:48 -0500

    Updating per feedback

--- functions/src/intentHandlers/localRestart.ts
+++ functions/src/intentHandlers/localRestart.ts
@@ -1,10 +1,17 @@
 import { Suggestion } from 'dialogflow-fulfillment'
 
 const isHomeEnabled = (agent) => {
-  return agent.context.get('cse-enableHome') !== undefined
-    || agent.context.get('snap-enableHome') !== undefined
-    || agent.context.get('tanf-enableHome') !== undefined
-    || agent.context.get('wfd-enableHome') !== undefined
+  return agent.context.get('cse-subject-matter') !== undefined
+    || agent.context.get('snap-subject-matter') !== undefined
+    || agent.context.get('tanf-subject-matter') !== undefined
+    || agent.context.get('wfd-subject-matter') !== undefined
+}
+
+const isRootIntent = (agent) => {
+  return agent.intent === 'cse-root'
+    || agent.intent === 'snap-root'
+    || agent.intent === 'tanf-root'
+    || agent.intent === 'wfd-root'
 }
 
 export const localRestart = async (agent, intentMap) => {
@@ -12,7 +19,7 @@ export const localRestart = async (agent, intentMap) => {
   const currentIntentFunction = intentMap[currentIntent]
   const localRestartFunction = async () => {
     await currentIntentFunction(agent)
-    if (isHomeEnabled(agent)) {
+    if (isHomeEnabled(agent) && !isRootIntent(agent)) {
       await agent.add(new Suggestion('Home'))
     }
   }
